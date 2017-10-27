7eb1e1e320a5301438f539ea9a945a5130336c1f
José Sánchez-Gallego <gallegoj@uw.edu>
2017-10-26 22:19:53 -0700

    additional tests for target
    target list for testing

--- lvmsurveysim/test/target/test_target.py
+++ lvmsurveysim/test/target/test_target.py
@@ -12,10 +12,25 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import copy
+import pathlib
+
 import pytest
 
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+import lvmsurveysim
 from lvmsurveysim.target import Target
 from lvmsurveysim.target import regions
+
+
+@pytest.fixture()
+def restore_config():
+
+    config_copy = copy.deepcopy(lvmsurveysim.config)
+    yield
+    lvmsurveysim.config = config_copy
 
 
 test_targets = [('Target1', (169, 35), 'circle', {'r': 0.1})]
@@ -27,6 +42,8 @@ def test_targets(target_name, coords, target_type, target_params):
     target = Target(target_name, coords, target_type, target_params)
     assert isinstance(target, Target)
 
+    assert target.name == target_name
+
     assert hasattr(target, 'region')
 
     if target_type == 'circle':
@@ -37,3 +54,34 @@ def test_targets(target_name, coords, target_type, target_params):
         assert isinstance(target.region, regions.PolygonalRegion)
     else:
         raise ValueError('invalid region type')
+
+
+def test_target_from_list(restore_config):
+
+    test_target_list = pathlib.Path(__file__).parents[1] / 'test_data/test_targets.yaml'
+
+    assert test_target_list.exists()
+
+    lvmsurveysim.config['target_list'] = str(test_target_list)
+
+    target_1 = Target.from_target_list('Target1')
+
+    assert target_1.name == 'Target1'
+    assert isinstance(target_1, Target)
+    assert isinstance(target_1.region, regions.EllipticalRegion)
+
+
+def test_plot_target_mollweide(plot):
+
+    target = Target('MyTarget', [(100, 65), (120, 70), (110, 70)], region_type='polygon')
+
+    assert isinstance(target.region, regions.PolygonalRegion)
+
+    fig, ax = target.plot(projection='mollweide')
+
+    if plot:
+        plot_path = pathlib.Path(__file__).parents[1] / 'plots/test_mytarget_mollweide.pdf'
+        plt.savefig(str(plot_path))
+
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(plt, Axes)
