5e6d68b46cae979b5f3457b32177763ebe365cf8
José Sánchez-Gallego <gallegoj@uw.edu>
2017-10-27 13:41:57 -0700

    basic testing for tiling

---
+++ lvmsurveysim/test/tiling/test_tiling.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# @Author: José Sánchez-Gallego
+# @Date: Oct 27, 2017
+# @Filename: test_tiling.py
+# @License: BSD 3-Clause
+# @Copyright: José Sánchez-Gallego
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from lvmsurveysim.target import Target
+from lvmsurveysim.tiling import Tiling
+
+
+def test_tiling_no_ifu(test_target_file):
+
+    target_1 = Target.from_target_list('Target1', target_list=test_target_file)
+
+    assert isinstance(target_1, Target)
+
+    tiling = Tiling(target_1)
+
+    assert isinstance(tiling, Tiling)
+    assert len(tiling.tiles) == 0
