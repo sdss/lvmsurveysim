8ebe8ae7a90e5f50c4ea186b947a49720b7ed9ed
José Sánchez-Gallego <gallegoj@uw.edu>
2017-11-07 23:37:49 -0800

    More complete version of Tile, with changes to _create_shapely

--- lvmsurveysim/tiling/tiling.py
+++ lvmsurveysim/tiling/tiling.py
@@ -14,7 +14,13 @@ from __future__ import absolute_import
 
 import copy
 
+import numpy as np
+
 from astropy import coordinates as coo
+from astropy import units as uu
+
+import shapely.affinity
+import shapely.geometry
 
 from .ifu import IFU
 from ..target.target import Target
@@ -36,6 +42,9 @@ class Tile(object):
         coords (`~astropy.coordinates.SkyCoord` or tuple):
             The ``(RA, Dec)`` coordinates on which the centre of mass of the
             `.IFU` will be placed.
+        scale (float):
+            The plate scale, used to convert IFU physical units to on-sky
+            positions and angles. In units of arcsec/mm.
         angle (`~astropy.coordinates.Angle` or float):
             The rotation angle of the `.IFU` measured from North to East.
         plot_params (dict):
@@ -44,7 +53,7 @@ class Tile(object):
 
     """
 
-    def __init__(self, ifu, coords, angle=0, plot_params=None):
+    def __init__(self, ifu, coords, scale, angle=0, plot_params=None):
 
         assert isinstance(ifu, IFU), 'ifu is not a valid input type.'
         self.ifu = ifu
@@ -57,11 +66,48 @@ class Tile(object):
 
         self._plot_params = plot_params
 
+        self.shapely = self._create_shapely(scale)
+
     def __repr__(self):
 
         return (f'<Tile RA={self.coords.ra.deg:.3f}, '
                 f'Dec={self.coords.dec.deg:.3f}, '
                 f'angle={self.angle.deg:.1f}>')
+
+    def _create_shapely(self, scale):
+        """Creates a shapely region representing the IFU on the sky."""
+
+        if not isinstance(scale, uu.Quantity):
+            scale = scale * uu.arcsec / uu.mm
+
+        subifus_polygons = []
+
+        for subifu in self.ifu.subifus:
+
+            # The radius of each subifu (the central row) is 1 by definition.
+            subifu_size = (subifu.n_rows * subifu.n_fibres) / 1000. * uu.mm
+
+            # Scales shapely geometry to mm.
+            subifu_mm = shapely.affinity.scale(subifu.geometry,
+                                               subifu_size.to('mm').value,
+                                               subifu_size.to('mm').value, center=(0, 0))
+
+            # Applies the plate scale and RA correction
+            subifu_arcsec = shapely.affinity.scale(
+                subifu_mm, scale, scale / np.cos(np.radians(self.coords.dec.deg)),
+                center=(0, 0))
+
+            # Translates the IFU to its location on the region.
+            subifu_translated = shapely.affinity.translate(subifu_arcsec,
+                                                           self.coords.ra.deg,
+                                                           self.coords.dec.deg)
+
+            # Rotates the IFU
+            sub_ifu_rotated = shapely.affinity.rotate(subifu_translated, -self.angle.deg)
+
+            subifus_polygons.append(sub_ifu_rotated)
+
+        return shapely.geometry.MultiPolygon(subifus_polygons)
 
     def set_plot_params(self, **kwargs):
         """Sets the default plotting parameters for this tile."""
@@ -92,6 +138,8 @@ class Tiling(object):
     Parameters:
         target (`.Target`):
             The `.Target` to be tiled.
+        telescope (`~lvmsurveysim.telescope.Telescope`):
+            The telescope that will be used to tile the target.
         ifu (`.IFU` or None):
             The `.IFU` to be used as tiling unit. If ``None``, the IFU
             can be defined when calling the object. Otherwise, the tiling will
@@ -106,9 +154,10 @@ class Tiling(object):
         When a `.Tiling` is instatiated with a `.Target` and an `.IFU`, the
         tiling process is run on init ::
 
+            >>> apo1 = Telescope('APO-1m')
             >>> m81 = Target.from_target_list('M81')
             >>> mono = MonolithicIFU()
-            >>> m81_tiling = Tiling(m81, ifu=mono)
+            >>> m81_tiling = Tiling(m81, apo1, ifu=mono)
             >>> m81_tiling.tiles
             [<Tile RA=169.1, Dec=69.2, angle=3.01>, ...]
 
@@ -116,7 +165,7 @@ class Tiling(object):
         that case, the tiling is executed when the object is called with a
         valid `.IFU` ::
 
-            >>> m81_tiling = Tiling(m81)
+            >>> m81_tiling = Tiling(m81, apo1)
             >>> m81_tiling.tiles
             []
             >>> m81_tiling(mono)
@@ -125,10 +174,12 @@ class Tiling(object):
 
     """
 
-    def __init__(self, target, ifu=None):
+    def __init__(self, target, telescope, ifu=None):
 
         assert isinstance(target, Target), 'target is of invalid type.'
         self.target = target
+
+        self.telescope = telescope
 
         self.tiles = []
 
@@ -138,7 +189,7 @@ class Tiling(object):
 
     def __repr__(self):
 
-        return (f'<Tiling target={self.target.name!r}, tiles={self.tiles!r}>')
+        return f'<Tiling target={self.target.name!r}, tiles={self.tiles!r}>'
 
     def __call__(self, ifu):
         """Runs the tiling process using ``ifu`` as the tiling unit."""
@@ -146,12 +197,14 @@ class Tiling(object):
         self._ifu = ifu
 
         # First pass. We overtile target.
-        target_shapely = copy.deepcopy(self.target.region.shapely)
-        while not target_shapely.is_empty:
-            repr_point = target_shapely.representative_point()
-            new_tile = Tile(self.ifu, (repr_point.x, repr_point.y))
+        untiled_shapely = copy.deepcopy(self.target.region.shapely)
+
+        while not untiled_shapely.is_empty:
+            repr_point = untiled_shapely.representative_point()
+            new_tile = Tile(self.ifu, (repr_point.x, repr_point.y),
+                            self.telescope.plate_scale.to('arcsec/mm'))
             self.tiles.append(new_tile)
-            target_shapely = target_shapely.difference(new_tile.ifu.polygon)
+            untiled_shapely = untiled_shapely.difference(new_tile.ifu.polygon)
 
     def plot(self, **kwargs):
         """Plots the tiles on the target region.
@@ -169,7 +222,12 @@ class Tiling(object):
 
         """
 
-        pass
+        fig, ax = self.target.plot(**kwargs)
+
+        for tile in self.tiles:
+            tile.plot(ax)
+
+        return fig, ax
 
     @property
     def ifu(self):
