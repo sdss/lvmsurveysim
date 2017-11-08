370e3462b8e4a233cb65c2e42ff72c8c2282c21a
José Sánchez-Gallego <gallegoj@uw.edu>
2017-11-07 23:55:16 -0800

    Fixed test_tiling

--- lvmsurveysim/test/tiling/test_tiling.py
+++ lvmsurveysim/test/tiling/test_tiling.py
@@ -13,16 +13,18 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from lvmsurveysim.target import Target
+from lvmsurveysim.telescope import Telescope
 from lvmsurveysim.tiling import Tiling
 
 
 def test_tiling_no_ifu(test_target_file):
 
     target_1 = Target.from_target_list('Target1', target_list=test_target_file)
+    telescope = Telescope('APO-1m')
 
     assert isinstance(target_1, Target)
 
-    tiling = Tiling(target_1)
+    tiling = Tiling(target_1, telescope)
 
     assert isinstance(tiling, Tiling)
     assert len(tiling.tiles) == 0
