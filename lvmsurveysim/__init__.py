8ddac2e54be0d9a50d0ce37e58532d8e07b24078
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-08 20:37:19 -0700

    Clean __init__.py

--- lvmsurveysim/__init__.py
+++ lvmsurveysim/__init__.py
@@ -1,10 +1,18 @@
-
-__all__ = ['config']
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2018-07-08
+# @Filename: __init__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+#
+# @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Last modified time: 2018-07-08 20:36:41
 
 from .utils.config import get_config
-# from .utils.logger import get_log
+from .utils.logger import log
 
-# log = get_log('lvmsurveysim', log_file_path='~/.lvm/lvmsurveysim.log')
+
 config = get_config('~/.lvm/lvmsurveysim.yaml')
 
 
