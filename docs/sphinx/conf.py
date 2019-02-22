0f291ba59159dab544b62c220550aaf8b4e62140
José Sánchez-Gallego <gallegoj@uw.edu>
2019-02-21 19:00:37 -0800

    Remove m2r dependency in docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -37,8 +37,7 @@ extensions = [
     'sphinx.ext.todo', 'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
-    'sphinx.ext.intersphinx', 'm2r'
-]
+    'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +45,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 # source_suffix = '.rst'
 
 # The master toctree document.
