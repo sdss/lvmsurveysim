0b5171ad4101ca4d0073a9f74c720e32eb1a9d50
José Sánchez-Gallego <gallegoj@uw.edu>
2019-02-24 13:03:53 -0800

    Import IFU and Telescope at top of namespace

--- lvmsurveysim/__init__.py
+++ lvmsurveysim/__init__.py
@@ -7,13 +7,19 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-02-21 14:07:33
+# @Last modified time: 2019-02-21 18:47:20
+
+# flake8: noqa
 
 from .utils.config import get_config
-from .utils.logger import log  # noqa
+from .utils.logger import log
 
 
 config = get_config('~/.lvm/lvmsurveysim.yaml')
+
+
+from .ifu import *
+from .telescope import *
 
 
 __version__ = '0.1.0dev'
