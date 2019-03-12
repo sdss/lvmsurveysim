a9ba8cbb2ae1696b0a5b9edf3e580e21eac5b88d
José Sánchez-Gallego <gallegoj@uw.edu>
2019-03-12 11:05:00 -0700

    Add custom exceptions and warnings

---
+++ lvmsurveysim/exceptions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2019-03-12
+# @Filename: exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+#
+# @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Last modified time: 2019-03-12 11:04:45
+
+
+class LVMSurveySimWarning(UserWarning):
+    pass
+
+
+class LVMSurveySimError(Exception):
+    pass
