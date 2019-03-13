ea469f934c20d3de197879bab621fc6460ab2133
José Sánchez-Gallego <gallegoj@uw.edu>
2019-03-12 17:13:21 -0700

    np -> numpy

--- lvmsurveysim/utils/spherical.py
+++ lvmsurveysim/utils/spherical.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 #
-# @Author: José Sánchez-Gallego
-# @Date: Oct 20, 2017
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2017-10-20
 # @Filename: spherical.py
-# @License: BSD 3-Clause
-# @Copyright: José Sánchez-Gallego
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+#
+# @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Last modified time: 2019-03-12 17:13:11
+
+import numpy
 
 
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-
-import numpy as np
-
-
-__all__ = ['great_circle_distance', 'ellipse_bbox']
+__all__ = ['great_circle_distance', 'ellipse_bbox', 'get_lst', 'get_altitude']
 
 
 def great_circle_distance(ra0, dec0, ra1, dec1):
@@ -29,10 +26,10 @@ def great_circle_distance(ra0, dec0, ra1, dec1):
 
     """
 
-    return np.rad2deg(
-        np.arccos(np.cos(np.deg2rad(dec0)) * np.cos(np.deg2rad(dec1)) *
-                  np.cos(np.deg2rad(ra1 - ra0)) +
-                  np.sin(np.deg2rad(dec0)) * np.sin(np.deg2rad(dec1))))
+    return numpy.rad2deg(
+        numpy.arccos(numpy.cos(numpy.deg2rad(dec0)) * numpy.cos(numpy.deg2rad(dec1)) *
+                     numpy.cos(numpy.deg2rad(ra1 - ra0)) +
+                     numpy.sin(numpy.deg2rad(dec0)) * numpy.sin(numpy.deg2rad(dec1))))
 
 
 def ellipse_bbox(ra, dec, a, b, pa, padding=0):
@@ -43,7 +40,18 @@ def ellipse_bbox(ra, dec, a, b, pa, padding=0):
 
     """
 
-    pa_rad = np.deg2rad(pa)
+    pa_rad = numpy.deg2rad(pa)
+
+    a_x = a * numpy.sin(pa_rad)
+    a_y = a * numpy.cos(pa_rad)
+    b_x = b * numpy.cos(pa_rad)
+    b_y = b * numpy.sin(pa_rad)
+
+    ra_delta = numpy.sqrt(a_x**2 + b_x**2) / numpy.cos(numpy.deg2rad(dec))
+    dec_delta = numpy.sqrt(a_y**2 + b_y**2)
+
+    return (numpy.array([ra - ra_delta - padding, ra + ra_delta + padding]),
+            numpy.array([dec - dec_delta - padding, dec + dec_delta + padding]))
 
     a_x = a * np.sin(pa_rad)
     a_y = a * np.cos(pa_rad)
