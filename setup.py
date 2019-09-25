a6b02e34856b681393ce8e6523681b83a603c9d4
José Sánchez-Gallego <gallegoj@uw.edu>
2019-09-25 15:48:42 -0700

    Bump version: 0.2.0dev → 0.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = [line.strip().replace('==', '>=') for line in open(requiremen
 
 NAME = 'lvmsurveysim'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
-VERSION = '0.2.0dev'
+VERSION = '0.2.0'
 RELEASE = 'dev' not in VERSION
 
 
