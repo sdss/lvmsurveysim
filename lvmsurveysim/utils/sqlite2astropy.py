64d0c1823468c5ea8bea739bbdcb1d1fe41f656d
Niv Drory <drory@astro.as.utexas.edu>
2021-08-20 20:47:01 -0500

    add infrastructure to connect to SQL db using peewee

---
+++ lvmsurveysim/utils/sqlite2astropy.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: Niv Drory (drory@astro.as.utexas.edu)
+# @Filename: tiledb.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+#
+
+
+# Very quick hack to load an sqlite3 table using peewee into as astropy.Table object
+# and save an astropy.Table into an sqlite3 database using a matching peewee model
+
+import astropy.table
+from peewee import *
+
+
+def peewee2astropy(model):
+   '''
+   Create an `astropy.table.Table` object using a peewee database
+   model class. Data are read from the corresponding SQL database.
+
+   Parameters
+   ----------
+   model : ~peewee.Model
+      The model corresponding to the table to be read
+
+   Returns
+   -------
+   table : ~astropy.table.Table 
+      astropy Table object with the data from the database table. Column
+      names match the attribute names in the model.
+   '''
+   cols = model._meta.database.get_columns(model._meta.table_name)
+   cnames = [c.name for c in cols]
+
+   table = astropy.table.Table()
+
+   rows = model.select()
+   inserts = []
+   for r in rows:
+      inserts.append([*iter(r.__data__.values())])
+
+   table.add_columns(list(zip(*inserts)), names=cnames)
+
+   return table
+
+
+def astropy2peewee(table, model, replace=False):
+   '''
+   Save an `astropy.table.Table` into a pewwee SQL database using a 
+   given model. The model's attributes MUST match the table's column by name and type.
+   Optionally insert or replace rows in the database.
+
+   Parameters
+   ----------
+   table : ~astropy.table.Table 
+      astropy Table object with the data to be stored. Columns
+      must match the attribute names in the model exactly.
+
+   model : ~peewee.Model
+      The peewee model of the database table to store into
+
+   replace : Boolean
+      Replace rather than insert into the SQL table. Defaults to False.
+   '''
+   cols = model._meta.database.get_columns(model._meta.table_name)
+   dbc = [c.name for c in cols]
+   tbc = table.colnames
+   assert (len(dbc) == len(tbc)) & (set(dbc) == set(tbc)), "sets of columns do not match."
+   if(replace==True):
+      model.replace_many(table).execute()
+   else:
+      model.insert_many(table).execute()
+
